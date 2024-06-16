82eb1dacbbba76d64bf88147cdd9a4202d928b2e
zhengzhenfu <zhengzf90@163.com>
2024-06-16 21:30:45 +0800

    修改启动类

--- examples/main_window.py
+++ examples/main_window.py
@@ -1,17 +1,12 @@
-
-from howtrader.event import EventEngine
-
-from howtrader.trader.engine import MainEngine
-from howtrader.trader.ui import MainWindow, create_qapp
-
-from howtrader.gateway.binance import BinanceSpotGateway, BinanceUsdtGateway, BinanceInverseGateway
-
+from howtrader.app.cta_backtester import CtaBacktesterApp
 from howtrader.app.cta_strategy import CtaStrategyApp
 from howtrader.app.data_manager import DataManagerApp
 from howtrader.app.data_recorder import DataRecorderApp
-from howtrader.app.algo_trading import AlgoTradingApp
-from howtrader.app.risk_manager import RiskManagerApp
-from howtrader.app.spread_trading import SpreadTradingApp
+from howtrader.event import EventEngine
+from howtrader.gateway.binance import BinanceSpotGateway, BinanceUsdtGateway, BinanceInverseGateway
+from howtrader.trader.engine import MainEngine
+from howtrader.trader.ui import MainWindow, create_qapp
+
 
 def main():
     """"""
@@ -25,6 +20,7 @@ def main():
     main_engine.add_gateway(BinanceSpotGateway)
     main_engine.add_gateway(BinanceUsdtGateway)
     main_engine.add_gateway(BinanceInverseGateway)
+    main_engine.add_app(CtaBacktesterApp)
     main_engine.add_app(CtaStrategyApp)
     main_engine.add_app(DataManagerApp)
     # main_engine.add_app(AlgoTradingApp)
